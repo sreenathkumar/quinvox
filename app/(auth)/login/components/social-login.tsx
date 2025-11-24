a01b28effe5287bbb5d5177294956edadb06ceef
Sreenath <sreenathkumar66@gmail.com>
2025-11-24 16:48:32 +0600

    fix: removed the facebook login

--- app/(auth)/login/components/social-login.tsx
+++ app/(auth)/login/components/social-login.tsx
@@ -61,15 +61,6 @@ function SocialLogin() {
             </Button>
 
             <Button
-                onClick={() => handleSocialSignup('Facebook')}
-                variant="outline"
-                className="w-full border-border hover:bg-muted text-foreground"
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4"><path strokeDasharray="24" strokeDashoffset="24" d="M17 4l-2 0c-2.5 0 -4 1.5 -4 4v12"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.4s" values="24;0" /></path><path strokeDasharray="8" strokeDashoffset="8" d="M8 12h7"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.5s" dur="0.2s" values="8;0" /></path></g></svg>
-                Facebook
-            </Button>
-
-            <Button
                 onClick={() => handleSocialSignup('Linkedin')}
                 variant="outline"
                 className="w-full border-border hover:bg-muted text-foreground"
